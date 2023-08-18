56a1522341e8cc9a217deb06da8707d18d13eb3d
SyarifBeginer <142413772+SyarifBeginer@users.noreply.github.com>
2023-08-18 21:03:23 +0700

    Update login-script.js
    update coment

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,30 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/* Membuat variebel setiap halaman view*/
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+//memebuat variabel informasi email dan password
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
-
+ 
+/* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+    /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+         /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
+       if (email == expectedEmail && password == expectedPassword) {
+        /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
+        goToHome();
+/* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+    } else {
+        showPopUp();
+    }
 });
